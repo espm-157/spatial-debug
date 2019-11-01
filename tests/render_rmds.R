bdd5af1f6e1e870e5721feb6217e2a1658eff23e
Carl Boettiger <cboettig@gmail.com>
2019-11-01 08:31:49 -0700

    more minimal example of error

--- tests/render_rmds.R
+++ tests/render_rmds.R
@@ -2,4 +2,7 @@
 ## call rmarkdown on all .Rmd files
 f <- list.files(recursive = TRUE)
 Rmds <- f[grepl(".Rmd$", f)]
+## This is OKAY!
+#lapply(Rmds, function(x) rmarkdown::render(x))
+## This breaks if `mean` is not specified as `raster::mean()`
 lapply(Rmds, rmarkdown::render)
